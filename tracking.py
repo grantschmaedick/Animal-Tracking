a70c4c63d859027ef20b48bd6a75d67f40ca5169
grantschmaedick <grantschmaedick@mymail.mines.edu>
2019-10-30 20:05:56 -0600

    Worked through some bugs and got the start of the script to run

--- tracking.py
+++ tracking.py
@@ -1,6 +1,8 @@
 import numpy as np
-from irl_imitation/mdp/gridworld.py import *
-from irl_imitation/mdp/value_iteration.py import *
+
+from irl_imitation.mdp.gridworld import *
+from irl_imitation.mdp.value_iteration import *
+
 
 H = 10
 W = 10
@@ -14,14 +16,14 @@ def main():
     rmap_gt = np.zeros([H, W])
     rmap_gt[H-1, W-1] = 1
     rmap_gt[0, W-1] = 1
-    rmpap_gt[H-1, 0] = 1
+    rmap_gt[H-1, 0] = 1
 
-    gw = gridworld.GridWorld(rmap_gt, {}, .5)
+    gw = GridWorld(rmap_gt, {}, .5)
 
     rewards_gt = np.reshape(rmap_gt, H*W, order='F')
 
     P_a = gw.get_transition_mat()
 
-    values_gt, policy_gt = value_iteration.value_iteration(P_a, rewards_gt, GAMMA, error=0.01, deterministic = True)
+    values_gt, policy_gt = value_iteration(P_a, rewards_gt, GAMMA, error=0.01, deterministic = True)
 
 main()
