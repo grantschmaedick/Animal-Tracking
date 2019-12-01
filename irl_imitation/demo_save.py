6525a0fe16406c2dd90ea4f6e6cc00cff9407792
awohlford <34891927+awohlford@users.noreply.github.com>
2019-11-30 20:33:20 -0700

    Update demo_save.py

--- irl_imitation/demo_save.py
+++ irl_imitation/demo_save.py
@@ -121,9 +121,9 @@ def main():
       reward = rmap_gt[int(next_loc[0]), int(next_loc[1])]
       is_done = np.array_equal(next_loc, terminal_state)
 
-      trajs.append(Step(cur_state=loc,
+      trajs.append(Step(cur_state=gw.idx2pos(loc),
                         action=action,
-                        next_state=next_loc,
+                        next_state=gw.idx2pos(next_loc),
                         reward=reward,
                         done=is_done))
 
