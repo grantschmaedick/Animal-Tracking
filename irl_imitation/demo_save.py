622c7204309983623983633244099511061c3a1d
awohlford <34891927+awohlford@users.noreply.github.com>
2019-12-01 12:29:53 -0700

    Update demo_save.py

--- irl_imitation/demo_save.py
+++ irl_imitation/demo_save.py
@@ -131,7 +131,7 @@ def main():
   print 'LP IRL training ..'
   rewards_lpirl = lp_irl(P_a, policy_gt, gamma=0.3, l1=20, R_max=R_MAX)
   print 'Max Ent IRL training ..'
-  rewards_maxent = maxent_irl(feat_map, P_a, GAMMA, trajs, LEARNING_RATE, N_ITERS)
+  rewards_maxent = maxent_irl(coast_map, P_a, GAMMA, trajs, LEARNING_RATE, N_ITERS)
 #   print 'Deep Max Ent IRL training ..'
 #   rewards = deep_maxent_irl(feat_map, P_a, GAMMA, trajs, LEARNING_RATE, N_ITERS)
   
